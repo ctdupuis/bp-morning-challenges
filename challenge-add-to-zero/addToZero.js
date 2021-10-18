403353361aa3a4e140cb82f26e97190ca3494bbb
ctdupuis <ctdupuis7@gmail.com>
2021-10-18 09:33:19 -0500

    challenge solved

---
+++ challenge-add-to-zero/addToZero.js
@@ -0,0 +1,19 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, 12]
+
+// Write your solution below:
+let result = false;
+
+for (let i = 0; i < array.length; i++) {
+    for (let x = 0; x < array.length; x++) {
+        if (i != x) {
+            if (array[i] + array[x] === 0) {
+                result = true
+            }
+        }
+    }
+}
+
+
+
+console.log(result)
