8caa83c523ce7b35dd0fc9b6fa16c6c4024e6841
ctdupuis <ctdupuis7@gmail.com>
2021-10-22 12:25:41 -0500

    pig latinized challenge

---
+++ chal-pig-latin/pigLatin.js
@@ -0,0 +1,20 @@
+// Write your solution below:
+// Paired with Immanuel Mamou
+
+
+const pigLatinize = sentence => {
+    let vowelsArr = ["a", "e", "i", "o", "u"]
+    let words = sentence.split(" ")
+
+    let latinized = words.map(word => {
+        if (vowelsArr.indexOf(word[0]) !== -1) {
+            return word + "yay"
+        } else {
+            return word.slice(1, word.length) + word.slice(0, 1) + "ay"
+        }
+    }).join(" ")
+    return latinized
+}
+
+
+console.log(pigLatinize("porcupines are cute"))
