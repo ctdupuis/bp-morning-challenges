9b2f0bb1fa4d7868644cf93ac9259d312bf8653b
ctdupuis <ctdupuis7@gmail.com>
2021-10-26 09:31:37 -0500

    changed variable name, solved again using .map

--- challenge-stock-prices/stockPrices.js
+++ challenge-stock-prices/stockPrices.js
@@ -1,15 +1,25 @@
 // Paired with Jagruti Depan
 
 const best = prices => {
-    let start = prices[0];
-    let profits = [];
-    for (let i = 0; i < prices.length; i++) {
-        if (prices[i] < start) {
-            start = prices[i]
+    let buyingPrice = prices[0];
+    // let profits = [];
+    // for (let i = 0; i < prices.length; i++) {
+    //     if (prices[i] < start) {
+    //         start = prices[i]
+    //     }
+    //     profits.push(prices[i] - start)
+    // }
+    let profits = prices.map(price => {
+        if (price < buyingPrice) {
+            buyingPrice = price
         }
-        profits.push(prices[i] - start)
-    }
+        return price - buyingPrice
+    })
     return Math.max(...profits);
 }
 
 console.log(best([5, 4, 3, 2, 1]))
+console.log(best([15, 10, 20, 22, 1, 9]))
+console.log(best([7, 9, 5, 6, 3, 2]))
+console.log(best([0, 100]))
+console.log(best([5, 4 , 3, 2, 1]))
