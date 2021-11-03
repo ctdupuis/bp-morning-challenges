5472c89c292e5620ff10e7a9f7b00aef883cf4fe
Cody <relationalprogrammer@gmail.com>
2021-11-03 09:11:24 -0500

    challenge balanced

---
+++ chal-balanced-parenthesis/balancedParens.js
@@ -0,0 +1,30 @@
+// Sample Strings
+let sample1 = "This ( is unbalanced."
+let sample2 = "(This (is (a) balanced) string.)"
+let sample3 = "This is () also ) unbalanced."
+let sample4 = "Balanced."
+
+// Write your solution below:
+
+const balanced = string => {
+    const splitStr = string.split("")
+    const leftSide = [];
+    const rightSide = [];
+
+    for (let i = 0; i < splitStr.length; i++) {
+        if (splitStr[i] === "(") {
+            leftSide.push(splitStr[i])
+        };
+        if (splitStr[i] === ")") {
+            rightSide.push(splitStr[i])
+        };
+    }
+
+    return leftSide.length === rightSide.length ? "balanced" : "unbalanced";
+}
+
+
+console.log(balanced(sample1))
+console.log(balanced(sample2))
+console.log(balanced(sample3))
+console.log(balanced(sample4))
